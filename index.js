23465c9f14e585c5dc7caf660fd858779e3cfc34
darnell018 <corey.darnell@ufl.edu>
2021-06-28 00:11:13 -0400

    Added JS to pull API data to HTML

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ weatherForm.addEventListener('submit', e => {
 function getWeather() {
   var cityName = document.getElementsByTagName("input")[0].value; //defines cityName for the url
 
-  var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=8b59d14e29c359a46c0a13a11ea24d86' //builds the url based on user input
+  var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=8b59d14e29c359a46c0a13a11ea24d86' //builds the url based on user input
 
   fetch(weatherUrl) //makes request
     .then(function(res) {
@@ -28,5 +28,42 @@ function getWeather() {
     .then(function(res) {
       console.log(res) // when the json is converted, log it
       // our code using the received data MUST happen here inside the .then statement
-    }) 
+      displayWeather(res)
+    });
   }
+
+  /*
+    Name of the Searched City with Country Code
+    Description of current weather
+    Icon image for current weather conditions
+    Current Temperature
+    Current "feels like" Temperature
+  */
+
+function displayWeather(res) {
+  //pulling the needed weather data
+  var theCity = res.name +', '+ res.sys.country;
+  var theCondtions = res.weather[0].description;
+  var weatherIconUrl = 'https://openweathermap.org/img/wn/' + res.weather[0].icon + '@2x.png'
+  var theWeatherIcon = weatherIconUrl;
+  var currentTemp = Math.round(res.main.temp);
+  var feelsLikeTemp = Math.round(res.main.feels_like);
+
+  var weatherDiv = document.getElementById('weather'); //gets the div already in the HTML
+  var data = document.createElement("li");
+  data.classList.add("city");
+  var weatherData = `
+    <h2 class="city">${theCity}</h2>
+
+    <figure>
+      <img class="icon" src=${theWeatherIcon}>
+      <figcaption>${theCondtions}</figcaption>
+    </figure>
+
+    <p class="temps">Currently: ${currentTemp}<sup>°F</sup></p>
+    <p class="temps">Feels Like: ${feelsLikeTemp}<sup>°F</sup></p>
+  `;
+  data.innerHTML = weatherData;
+  weatherDiv.appendChild(data);
+
+}
